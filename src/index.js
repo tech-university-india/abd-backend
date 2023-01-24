6f5a94d86624a5f6daec2a59984b6485756d7de1
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-01-24 16:30:25 +0530

    chore: set up logger

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 const { PORT } = require('./config');
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({'extended': false}));
 
