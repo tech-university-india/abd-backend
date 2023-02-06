9a486b783c80c1b7fd6ecf771cdaf5361152ddeb
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-02-06 10:42:02 +0530

    fix: add cors for any domain for now

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const { PORT } = require('./config');
 const { errorHandlingMiddleware } = require('./middlewares');
+var cors = require('cors');
 
 require('dotenv').config();
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ 'extended': false }));
