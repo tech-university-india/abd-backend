e884d40a999bacf2d6e924be7155e3e16c38b338
Bharath Raj GN <86262066+bharathraj25@users.noreply.github.com>
2023-02-12 18:45:04 +0530

    fix: restructured some files, functions & folders. (#14)
    * fix: create different file for prisma client
    
    * fix: move some functions to prisma utils file
    
    * fix: remove unwanted folders
    
    * fix: remove destructuring for unit testing (jest)
    
    * fix: resolve prisma create po note bug

--- src/utils/prismaUtils.js
+++ src/utils/prismaUtils.js
@@ -6,6 +6,48 @@ const getPaginationObject = (page, limit) => {
   } : null;
 };
 
+// get formated date range object to filter notes,
+// that will be used in the query
+const getDateRangeObject = (startDate, endDate) => {
+
+  const sDate = new Date(startDate);
+  const eDate = endDate ?
+    new Date(endDate) :
+    new Date(startDate);
+
+  eDate.setDate(eDate.getDate() + 1);
+
+  return {
+    createdAt: {
+      gte: sDate,
+      lt: eDate
+    }
+  };
+};
+
+// get formated search object to filter notes,
+// that will be used in the query
+const getSearchKeywordObject = (searchKeyword) => {
+  return {
+    note: {
+      contains: searchKeyword,
+      mode: 'insensitive',
+    }
+  };
+};
+
+// get formated status object to filter notes,
+// that will be used in the query
+const getStatusQueryObject = (status) => {
+  status = status.toUpperCase();
+  return {
+    status
+  };
+};
+
 module.exports = {
   getPaginationObject,
+  getStatusQueryObject,
+  getSearchKeywordObject,
+  getDateRangeObject
 };
