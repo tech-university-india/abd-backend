73b073a1db32d54beacbed0fb49f75437adf2889
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-02-16 02:59:15 +0530

    feat: add routes for dsm-celebrations endpoints

---
+++ src/routes/api/dsmSection/dsmSection.celebrationBoard.routes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const {
+  listCelebrations,
+  detailCelebration,
+  createCelebration,
+  updateCelebration,
+  deleteCelebration
+} = require('../../../controllers/dsmSection/dsmSection.celebrationBoard.controller');
+
+// GET /api/dsm-celebration
+router.get('/', listCelebrations);
+
+// POST /api/dsm-celebration
+router.post('/', createCelebration);
+
+// GET /api/dsm-celebration/:id
+router.get('/:id', detailCelebration);
+
+// PUT /api/dsm-celebration/:id
+router.put('/:id', updateCelebration);
+
+// DELETE /api/dsm-celebration/:id
+router.delete('/:id', deleteCelebration);
+
+module.exports = router;
+
+
