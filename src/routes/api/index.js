455dbd95059c890db508cbe8f185dbe4d2d9108e
karthik01455 <karthikshriram4509@gmail.com>
2023-02-18 20:35:39 +0530

    feat: add routes for dsm team-requests

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.use('/po-notes', require('./poNotes.routes'));
-
+router.use('/dsm/team-requests',require('./dsm/teamRequests.routes'))
 module.exports = router;
