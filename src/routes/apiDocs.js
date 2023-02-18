604f19ba32385a29eff1ae99071ff1bd3b0675bf
karthik01455 <karthikshriram4509@gmail.com>
2023-02-18 20:39:28 +0530

    fix: removed extra spaces

--- src/routes/apiDocs.js
+++ src/routes/apiDocs.js
@@ -1,8 +1,5 @@
 const router = require('express').Router();
-
 const swaggerUi = require('swagger-ui-express');
 const {swaggerSpec} = require('../swagger');
-
 router.use('', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 module.exports = router;
