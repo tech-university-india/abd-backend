7fc365e6a959c7e8285d3e7075e66cb415eecf7b
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-02-18 20:46:22 +0530

    chore: swagger documentation for celebration-board schema

--- src/routes/api/dsmSection/dsmSection.celebrationBoard.routes.js
+++ src/routes/api/dsmSection/dsmSection.celebrationBoard.routes.js
@@ -7,6 +7,56 @@ const {
   deleteCelebration
 } = require('../../../controllers/dsmSection/dsmSection.celebrationBoard.controller');
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Celebration:
+ *       type: object
+ *       required:
+ *         - celebrationId
+ *         - author
+ *         - content
+ *         - createdAt
+ *       properties:
+ *         celebrationId:
+ *           type: integer
+ *           description: Unique identifier of the celebration
+ *         author:
+ *           type: string
+ *           description: Author of the celebration
+ *         content:
+ *           type: string
+ *           description: Content of the celebration
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *           description: Created at
+ */
+
+
+/**
+ * @openapi
+ * /api/dsm-celebration:
+ *   get:
+ *     summary: Get all celebrations
+ *     tags:
+ *       - dsm-celebration
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Celebration'
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+
 // GET /api/dsm-celebration
 router.get('/', listCelebrations);
 
