0ad2b7bcd1d995b56370550ffba490f3b14740d0
kartik162000 <kartikg.goel@gmail.com>
2023-02-20 20:37:13 +0530

    feat: API endpoints for dsm/sentiment-meter/ with validations and swagger documentation

--- src/routes/api/dsm/sentimentMeter.routes.js
+++ src/routes/api/dsm/sentimentMeter.routes.js
@@ -10,18 +10,209 @@ const {
   generateValidationMiddleware,
 } = require("../../../middlewares/validation");
 const sentimentMeterSchema = require("../../../schemas/dsm/sentimentMeterSchema");
+
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     SentimentMeter:
+ *       type: object
+ *       required:
+ *         - sentimentMeterId
+ *         - author
+ *         - sentiment
+ *         - createdAt
+ *       properties:
+ *         sentimentMeterId:
+ *           type: integer
+ *           description: Unique identifier of the sentimentMeter
+ *         author:
+ *           type: integer
+ *           description: Unique identifier of the author
+ *         sentiment:
+ *           type: string
+ *           description: Sentiment of the author
+ *           enum:
+ *             - HAPPY
+ *             - SAD
+ *             - OK
+ *             - BAD
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *           description: Created at
+ */
+
+/**
+ * @openapi
+ * /api/dsm-sentiment-meter:
+ *   get:
+ *     summary: Get all sentimentMeter
+ *     tags:
+ *       - sentimentMeter
+ *     description: Get all sentiments
+ *     responses:
+ *       200:
+ *         description: List of all sentiments
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/SentimentMeter'
+ *       500:
+ *         description: Internal server error
+ *   post:
+ *     summary: Create a sentimentMeter
+ *     tags:
+ *       - sentimentMeter
+ *     description: Create a sentimentMeter
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - author
+ *               - sentiment
+ *             properties:
+ *               author:
+ *                 type: integer
+ *                 description: Unique identifier of the author
+ *               sentiment:
+ *                 type: string
+ *                 description: Sentiment of the authors
+ *                 enum:
+ *                   - HAPPY
+ *                   - SAD
+ *                   - OK
+ *                   - BAD
+ *     responses:
+ *       201:
+ *         description: SentimentMeter created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/SentimentMeter'
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+
+router.get("/", getAllSentiment);
 router.post(
   "/",
   generateValidationMiddleware(sentimentMeterSchema.createSentiment),
   createSentiment
 );
-router.get("/", getAllSentiment);
+
+/**
+ * @openapi
+ * /api/dsm-sentiment-meter/date/{createdAt}:
+ *   get:
+ *     summary: Get sentimentMeter by date
+ *     tags:
+ *       - sentimentMeter
+ *     description: Get sentimentMeter by date
+ *     parameters:
+ *       - in: path
+ *         name: createdAt
+ *         schema:
+ *           type: string
+ *           format: date
+ *         required: true
+ *         description: Date of the sentimentMeter
+ *     responses:
+ *       200:
+ *         description: SentimentMeter by date
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/SentimentMeter'
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
 
 router.get(
   "/date/:createdAt",
   generateValidationMiddleware(sentimentMeterSchema.dateSchema, "params"),
   countSentimentByDate
 );
+
+/**
+ * @openapi
+ * /api/dsm-sentiment-meter/{id}:
+ *   get:
+ *     summary: Get sentimentMeter by id
+ *     tags:
+ *       - sentimentMeter
+ *     description: Get sentimentMeter by id
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Unique identifier of the sentimentMeter
+ *     responses:
+ *       200:
+ *         description: SentimentMeter by id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/SentimentMeter'
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ *   patch:
+ *     summary: Update sentimentMeter by id
+ *     tags:
+ *       - sentimentMeter
+ *     description: Update sentimentMeter by id
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Unique identifier of the sentimentMeter
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - sentiment
+ *             properties:
+ *               sentiment:
+ *                 type: string
+ *                 description: Sentiment of the authors
+ *                 enum:
+ *                   - HAPPY
+ *                   - SAD
+ *                   - OK
+ *                   - BAD
+ *     responses:
+ *       200:
+ *         description: SentimentMeter updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/SentimentMeter'
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+
 router.get(
   "/:id",
   generateValidationMiddleware(sentimentMeterSchema.getByIdSchema, "params"),
--- src/services/dsm/sentimentMeter.services.js
+++ src/services/dsm/sentimentMeter.services.js
@@ -1,97 +1,96 @@
-const { HttpError } = require('../../errors');
-const prisma = require('../../prismaClient');
+const { HttpError } = require("../../errors");
+const prisma = require("../../prismaClient");
 
 const selectOnlyValidSentimentMeterFields = {
-    select: {
-      sentimentMeterId: true,
-      author: true,
+  select: {
+    sentimentMeterId: true,
+    author: true,
+    sentiment: true,
+    createdAt: true,
+  },
+};
+const createSentiment = async (author, sentiment) => {
+  const newSentiment = await prisma.sentimentMeter.create({
+    data: {
+      author,
+      sentiment,
+    },
+    ...selectOnlyValidSentimentMeterFields,
+  });
+  return newSentiment;
+};
+
+const getSentimentById = async (sentimentMeterId) => {
+  const sentiment = await prisma.sentimentMeter.findUnique({
+    where: {
+      sentimentMeterId,
+    },
+    ...selectOnlyValidSentimentMeterFields,
+  });
+  if (!sentiment) {
+    throw new HttpError(404, `Sentiment with id ${sentimentMeterId} not found`);
+  }
+  return sentiment;
+};
+
+const updateSentimentById = async (sentimentMeterId, sentiment) => {
+  const updatedSentiment = await prisma.sentimentMeter.update({
+    where: {
+      sentimentMeterId,
+    },
+    data: {
+      sentiment,
+    },
+    ...selectOnlyValidSentimentMeterFields,
+  });
+  if (updatedSentiment.count === 0) throw new HttpError(404, "No Record Found");
+  return updatedSentiment;
+};
+
+//count of each sentiment by date
+const countSentimentByDate = async (createdAt) => {
+  const countSentiment = await prisma.sentimentMeter.groupBy({
+    by: ["sentiment"],
+    where: {
+      //greater than or equal to and less than or equal to next day
+      createdAt: {
+        gte: new Date(createdAt),
+        lte: new Date(
+          new Date(createdAt).setDate(new Date(createdAt).getDate() + 1)
+        ),
+      },
+    },
+    _count: {
       sentiment: true,
-      createdAt: true,
-    }
-  };
-  const createSentiment = async (author, sentiment) => {
-    const newSentiment = await prisma.sentimentMeter.create({
-      data: {
-        author,
-        sentiment,
-      },
-      ...selectOnlyValidSentimentMeterFields
-    }
-    );
-    return newSentiment;
-  };
+    },
+  });
 
-  const getSentimentById = async (sentimentMeterId) => {
-    const sentiment = await prisma.sentimentMeter.findUnique({
-      where: {
-        sentimentMeterId
-      },
-      ...selectOnlyValidSentimentMeterFields
-    });
-    if (!sentiment) {
-      throw new HttpError(404, `Sentiment with id ${id} not found`);
-    }
-    return sentiment;
-  };
+  if (countSentiment.length === 0)
+    throw new HttpError(404, "No Data found for the date" + createdAt);
 
+  const fullCount = countSentiment.reduce((acc, curr) => {
+    acc += curr._count.sentiment;
+    return acc;
+  }, 0);
 
-  const updateSentimentById = async (sentimentMeterId, sentiment) => {
-    const updatedSentiment = await prisma.sentimentMeter.update({
-      where: {
-        sentimentMeterId
-      },
-      data: {
-        sentiment,
-      },
-      ...selectOnlyValidSentimentMeterFields
-    }
-    );
-    if (updatedSentiment.count === 0) throw new HttpError(404, 'No Record Found');
-    return updatedSentiment;
-  };
+  countSentiment.forEach((element) => {
+    element.percentage = (element._count.sentiment / fullCount) * 100;
+    delete element._count;
+  });
+  return countSentiment;
+};
 
-  //count of each sentiment by date
-  const countSentimentByDate = async (createdAt) => {
-    const countSentiment = await prisma.sentimentMeter.groupBy({
-      by: ['sentiment'],
-      where: {
-        //greater than or equal to and less than or equal to next day
-        createdAt: {
-          gte: new Date(createdAt),
-          lte: new Date(new Date(createdAt).setDate(new Date(createdAt).getDate() + 1))
-        }
-      },
-      _count: {
-        sentiment: true
-      }
-    });
-    
-    if (countSentiment.length === 0) throw new HttpError(404, 'No Data found for the date' + createdAt);
+const getAllSentiment = async () => {
+  const allSentiment = await prisma.sentimentMeter.findMany({
+    ...selectOnlyValidSentimentMeterFields,
+  });
+  return allSentiment;
+};
 
-    const fullCount=countSentiment.reduce((acc, curr) => {
-    acc+=curr._count.sentiment;
-      return acc;
-    }, 0);
-
-    countSentiment.forEach((element) => {
-      element.percentage = (element._count.sentiment)/fullCount*100;
-      delete element._count;
-    }
-    );
-    return countSentiment;
-  };
-
-  const getAllSentiment = async () => { 
-    const allSentiment = await prisma.sentimentMeter.findMany({
-      ...selectOnlyValidSentimentMeterFields
-    });
-    return allSentiment;
-  };
-
-  module.exports = {
-    createSentiment,
-    getSentimentById,
-    updateSentimentById,
-    countSentimentByDate,
-    getAllSentiment
-    }
+module.exports = {
+  createSentiment,
+  getSentimentById,
+  updateSentimentById,
+  countSentimentByDate,
+  getAllSentiment,
+};
